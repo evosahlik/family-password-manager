9f80ac33b8f2d537f88b9897e3370fedd07cf1c5
Eric Vosahlik <eric.vosahlik@gmail.com>
2026-02-14 19:08:13 -0600

    Before last update I had to rebuild the Cognito User Pool and that required me to update my config.js IDs

--- frontend/password-manager-ui/src/config.js
+++ frontend/password-manager-ui/src/config.js
@@ -1,8 +1,8 @@
 const config = {
   cognito: {
     region: 'us-east-1', // Your AWS region
-    userPoolId: 'us-east-1_XLGNUlZkM', // From Monday's task
-    userPoolWebClientId: '593903apijinant9porklfp39c', // From Monday's task
+    userPoolId: 'us-east-1_rrhQZVbrG', // From Monday's task
+    userPoolWebClientId: '7pqb2m4c295mf11kmvojsgqq0h', // From Monday's task
   },
   api: {
     // We'll add this in Sprint 4
